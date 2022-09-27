47019860ffe53ddcbc1ba8420bed686ef69de473
krocks96 <norinorikuronori@gmail.com>
2022-09-28 01:02:47 +0900

    ログのカスタマイズ

--- backend/src/app/server/server.go
+++ backend/src/app/server/server.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"app/controllers"
+	"fmt"
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,8 +16,59 @@ func StartServer() {
 	r.Run()
 }
 
+// ログフォーマッタ
+var customLogFormatter = func(param gin.LogFormatterParams) string {
+	var statusColor, methodColor, resetColor string
+	if param.IsOutputColor() {
+		statusColor = param.StatusCodeColor()
+		methodColor = param.MethodColor()
+		resetColor = param.ResetColor()
+	}
+
+	if param.Latency > time.Minute {
+		param.Latency = param.Latency.Truncate(time.Second)
+	}
+
+	// ここでフォーマットを決めている
+	return fmt.Sprintf("[GIN] %v| %s |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		param.Request.Header.Get("X-Request-Id"), // ここに追加
+		statusColor,
+		param.StatusCode,
+		resetColor,
+		param.Latency,
+		param.ClientIP,
+		methodColor,
+		param.Method,
+		resetColor,
+		param.Path,
+		param.ErrorMessage,
+	)
+}
+
+// カスタムログ
+func customLogger() gin.LoggerConfig {
+	conf := gin.LoggerConfig{
+		SkipPaths: []string{"/api/v1/healthcheck"},
+	}
+	conf.Formatter = customLogFormatter
+	return conf
+}
+
+// カスタムハンドラ
+func customRequestidHandler() requestid.Handler {
+	return func(c *gin.Context, requestID string) {
+		c.Request.Header.Set("X-Request-Id", requestID)
+	}
+}
+
 func Router() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(requestid.New(
+		requestid.WithHandler(customRequestidHandler())),
+	)
+	r.Use(gin.LoggerWithConfig(customLogger()))
+	r.Use(gin.Recovery())
 	// CORS設定
 	r.Use(cors.New(cors.Config{
 		// 許可したいHTTPメソッドの一覧
@@ -39,7 +92,6 @@ func Router() *gin.Engine {
 		},
 		MaxAge: 24 * time.Hour,
 	}))
-	// ルーティングの設定
 	defineRoutes(r)
 	return r
 }
